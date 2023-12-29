6a4e353f89038ce0e0c21e937d3f820f0ebb92bd
Thomas Sun <sguoqing@gmail.com>
2023-12-29 15:16:08 +0800

    up to docs.pola.rs Expressions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
+// use chrono::prelude::*;
+use polars::lazy::dsl::*;
 use polars::prelude::*;
+use rand::{thread_rng, Rng};
 fn main() -> PolarsResult<()> {
-    let df = CsvReader::from_path("./dataset/apple_stock.csv")?
-        .has_header(true)
-        .finish()?;
-    println!("{:?}", df.describe(None));
+    let mut arr = [0f64; 5];
+    thread_rng().fill(&mut arr);
+    let df = df![
+        "nrs" => &[Some(1),Some(2),Some(3),None,Some(5)],
+        "names" => &[Some("foo"),Some("ham"),Some("spam"),Some("eggs"),None],
+        "random" => &arr,
+        "groups" => &["A","A","B","C","B"],
+
+    ]?;
+    let out = df
+        .clone()
+        .lazy()
+        .group_by([col("groups")])
+        .agg([
+            sum("nrs"),
+            col("random").count().alias("Count"),
+            col("random")
+                .filter(col("names").is_not_null())
+                .sum()
+                .name()
+                .suffix("_sum"),
+            // col("names").reverse().alias("reversed names"),
+            col("names").alias("reversed names"),
+        ])
+        .collect()?;
+    println!("{:?}", out);
     Ok(())
 }
