5bd5b8a4f20c9da91e0cd882f19db288fe5dced6
thomas.sun <sguoqing@gmail.com>
2023-12-30 20:50:14 +0800

    lazy csv reading

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 // use chrono::prelude::*;
 use polars::lazy::dsl::*;
-use polars::prelude::{Literal, *};
+use polars::prelude::*;
 // use rand::{thread_rng, Rng};
 fn main() -> PolarsResult<()> {
-    let df = CsvReader::from_path("./dataset/apple_stock.csv")
-        .unwrap()
+    let df = LazyCsvReader::new("./dataset/apple_stock.csv")
         .with_try_parse_dates(true)
-        .finish()
-        .unwrap()
-        .sort(["Date"], false, true)?;
+        .finish()?
+        .sort(
+            "Date",
+            SortOptions {
+                ..Default::default()
+            },
+        );
     let out = df
-        .clone()
-        .lazy()
         .group_by_dynamic(
             col("Date"),
             [],
@@ -31,7 +32,11 @@ fn main() -> PolarsResult<()> {
             // col("Date").first().alias("Start"),
             // col("Date").last().alias("End"),
         ])
+        // .explain(true)?;
+        .with_streaming(true)
         .collect()?;
+
     println!("{}", &out);
+    // println!("{}", &df.collect()?);
     Ok(())
 }
